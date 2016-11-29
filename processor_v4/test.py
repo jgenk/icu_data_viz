3da740782e8bc364be614ccf4881ac7d75f0ee96
Julian Genkins <jgenk90@gmail.com>
2016-11-29 16:18:29 -0600

    Flattened out feature rows. now have a dataframe that can be processed

--- processor_v4/test.py
+++ processor_v4/test.py
@@ -10,39 +10,36 @@ import pandas as pd
 from random import sample
 
 def main():
-    
     proc = dbp.Processor()
-    hadm_ids = get_hadm_ids(proc,20)
+    hadm_ids = get_hadm_ids(proc,5)
     feature_list = ["LACTATE","HEMOGLOBIN","OXYGEN_SATURATION"]
-    for hadm_id in hadm_ids:
-        df = create_df_for_features(proc, hadm_id, feature_list)
+    df = create_df_for_hadm_ids(proc, hadm_ids, feature_list)
     print df
-#    proc.timeseries_by_hadm_id(0, hadm_id)
-#    
-#    proc.get_context_info(190659)
-#    proc.hadm_ids(id_type="icustay",IDs=[216989])
-#    print proc.timeseries_by_hadm_id(1,139061)
-#    
-#    lactate_ts_list = proc.bulk_timeseries(0,randomize=True,data_limits=(10,None),ts_max=1) #Lactate
-#    pt_ids = [ts.context_df.PT_ID[0] for ts in lactate_ts_list]
-#    hr_ts_list = proc.bulk_timeseries(1,id_type='patient',IDs=pt_ids) #Lactate
-#    
-#    lactate_dict = {}
-#    for ts in lactate_ts_list: lactate_dict[ts.pt_id]=ts
-#    HR_dict = {}
-#    for ts in hr_ts_list: HR_dict[ts.pt_id]=ts
+    return
 
-
-def create_df_for_features(processor, hadm_id, feature_ids):
-    feat_dict = features_for_hadm_id(processor, hadm_id, feature_ids)
+def create_df_for_hadm_ids(proc, hadm_ids,feature_list):
     df = pd.DataFrame()
-    for key in feat_dict.keys():
-        ts = feat_dict[key]
-        ts_describe = pd.to_numeric(ts.series).describe()
-        print ts.series
-        print ts_describe
+    for hadm_id in hadm_ids:
+        row = row_for_features(proc, hadm_id, feature_list)
+        df = df.append(row)
     return df
 
+def row_for_features(processor, hadm_id, feature_ids):
+    feat_dict = features_for_hadm_id(processor, hadm_id, feature_ids)
+    describe_list = []
+    for key in feat_dict.keys():
+        ts = feat_dict[key]
+        ts_describe = convert_ts_to_row(ts)
+        describe_list.append(ts_describe)
+    df = pd.DataFrame(describe_list)
+    df = df.stack().to_frame().T
+    df.columns = ['{}_{}'.format(*c) for c in df.columns]
+    return df
+
+def convert_ts_to_row(ts):
+    #TODO: Make this more intelligent
+    return pd.to_numeric(ts.series).describe()
+    
 def plot_ts(ts):
     series = shift_ts_to_DT(ts, ts.context_df.hadm_admDT[0])
     series = pd.to_numeric(series)
@@ -67,3 +64,18 @@ def get_hadm_ids(proc,cnt):
     
 if __name__ == "__main__":
     main()
+    
+    #    proc.timeseries_by_hadm_id(0, hadm_id)
+#    
+#    proc.get_context_info(190659)
+#    proc.hadm_ids(id_type="icustay",IDs=[216989])
+#    print proc.timeseries_by_hadm_id(1,139061)
+#    
+#    lactate_ts_list = proc.bulk_timeseries(0,randomize=True,data_limits=(10,None),ts_max=1) #Lactate
+#    pt_ids = [ts.context_df.PT_ID[0] for ts in lactate_ts_list]
+#    hr_ts_list = proc.bulk_timeseries(1,id_type='patient',IDs=pt_ids) #Lactate
+#    
+#    lactate_dict = {}
+#    for ts in lactate_ts_list: lactate_dict[ts.pt_id]=ts
+#    HR_dict = {}
+#    for ts in hr_ts_list: HR_dict[ts.pt_id]=ts
